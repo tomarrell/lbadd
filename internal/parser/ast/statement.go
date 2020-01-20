9f46c74208cf54f643f5d62832adac14f6eefed2
Tim Satke <tim.satke@gmail.com>
2020-01-20 22:27:12 +0100

    Add tokens to AST nodes

--- internal/parser/ast/statement.go
+++ internal/parser/ast/statement.go
@@ -1,35 +1,44 @@
 package ast
 
-type SqlStmt struct {
-	Explain bool
-	Query   bool
-	Plan    bool
+import (
+	"github.com/tomarrell/lbadd/internal/parser/scanner/token"
+)
 
-	AlterTableStmt         *AlterTableStmt
-	AnalyzeStmt            *AnalyzeStmt
-	AttachStmt             *AttachStmt
-	BeginStmt              *BeginStmt
-	CommitStmt             *CommitStmt
-	CreateIndexStmt        *CreateIndexStmt
-	CreateTableStmt        *CreateTableStmt
-	CreateTriggerStmt      *CreateTriggerStmt
-	CreateViewStmt         *CreateViewStmt
-	CreateVirtualTableStmt *CreateVirtualTableStmt
-	DeleteStmt             *DeleteStmt
-	DeleteStmtLimited      *DeleteStmtLimited
-	DetachStmt             *DetachStmt
-	DropIndexStmt          *DropIndexStmt
-	DropTableStmt          *DropTableStmt
-	DropTriggerStmt        *DropTriggerStmt
-	DropViewStmt           *DropViewStmt
-	InsertStmt             *InsertStmt
-	PragmaStmt             *PragmaStmt
-	ReindexStmt            *ReindexStmt
-	ReleaseStmt            *ReleaseStmt
-	RollbackStmt           *RollbackStmt
-	SavepointStmt          *SavepointStmt
-	SelectStmt             *SelectStmt
-	UpdateStmt             *UpdateStmt
-	UpdateStmtLimited      *UpdateStmtLimited
-	VacuumStmt             *VacuumStmt
-}
+type (
+	SQLStmt struct {
+		Explain token.Token
+		Query   token.Token
+		Plan    token.Token
+
+		AlterTableStmt         *AlterTableStmt
+		AnalyzeStmt            *AnalyzeStmt
+		AttachStmt             *AttachStmt
+		BeginStmt              *BeginStmt
+		CommitStmt             *CommitStmt
+		CreateIndexStmt        *CreateIndexStmt
+		CreateTableStmt        *CreateTableStmt
+		CreateTriggerStmt      *CreateTriggerStmt
+		CreateViewStmt         *CreateViewStmt
+		CreateVirtualTableStmt *CreateVirtualTableStmt
+		DeleteStmt             *DeleteStmt
+		DeleteStmtLimited      *DeleteStmtLimited
+		DetachStmt             *DetachStmt
+		DropIndexStmt          *DropIndexStmt
+		DropTableStmt          *DropTableStmt
+		DropTriggerStmt        *DropTriggerStmt
+		DropViewStmt           *DropViewStmt
+		InsertStmt             *InsertStmt
+		PragmaStmt             *PragmaStmt
+		ReindexStmt            *ReindexStmt
+		ReleaseStmt            *ReleaseStmt
+		RollbackStmt           *RollbackStmt
+		SavepointStmt          *SavepointStmt
+		SelectStmt             *SelectStmt
+		UpdateStmt             *UpdateStmt
+		UpdateStmtLimited      *UpdateStmtLimited
+		VacuumStmt             *VacuumStmt
+	}
+
+	AlterTableStmt struct {
+	}
+)
