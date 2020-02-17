4747f514a2bf64e5c7ec410e0171971612e165ba
Tim Satke <tim.satke@gmail.com>
2020-02-17 16:56:26 +0100

    Create testable entry point

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/tomarrell/lbadd"
 )
 
 func main() {
+	if err := run(os.Args, os.Stdin, os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintf(os.Stdout, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string, stdin io.Reader, stdout, stderr io.Writer) error {
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	var (
+		verbose = flags.Bool("verbose", false, "enable verbose output")
+	)
+	_ = *verbose // TODO: use *verbose to configure a logger
+
+	if err := flags.Parse(args[1:]); err != nil {
+		return fmt.Errorf("parse flags: %w", err)
+	}
+
 	r := lbadd.NewRepl()
 	r.Start()
+
+	return nil
 }
