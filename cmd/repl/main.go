9399d4a8716512c31d7fe16bfdc71ec3a000ebc0
Tim Satke <tim.satke@gmail.com>
2020-02-18 09:03:18 +0100

    Fix unchecked error

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	if err := run(os.Args, os.Stdin, os.Stdout, os.Stderr); err != nil {
-		fmt.Fprintf(os.Stdout, "%s\n", err)
+		_, _ = fmt.Fprintf(os.Stdout, "%s\n", err)
 		os.Exit(1)
 	}
 }
