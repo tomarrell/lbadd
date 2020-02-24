939da99e0714857fa0a1a8371521c012ad19e184
Tim Satke <tim.satke@gmail.com>
2020-02-24 15:43:32 +0100

    Remove golden testing for parser

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,27 +1,13 @@
 package parser
 
 import (
-	"flag"
-	"os"
 	"testing"
 
-	"github.com/TimSatke/golden"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/tomarrell/lbadd/internal/parser/ast"
 	"github.com/tomarrell/lbadd/internal/parser/scanner/token"
 )
-
-var (
-	update bool
-)
-
-func TestMain(m *testing.M) {
-	flag.BoolVar(&update, "update", false, "enable to record tests and write results to disk as base for future comparisons")
-	flag.Parse()
-
-	os.Exit(m.Run())
-}
 
 func TestSingleStatementParse(t *testing.T) {
 	inputs := []struct {
@@ -185,32 +171,6 @@ func TestSingleStatementParse(t *testing.T) {
 
 			_, _, ok = p.Next()
 			assert.False(ok, "expected only one statement")
-		})
-	}
-}
-
-func TestParserGolden(t *testing.T) {
-	inputs := []struct {
-		Name  string
-		Query string
-	}{
-		{"empty", ""},
-	}
-	for _, input := range inputs {
-		t.Run(input.Name, func(t *testing.T) {
-			p := NewSimpleParser(input.Query)
-
-			for {
-				stmt, errs, ok := p.Next()
-				if !ok {
-					break
-				}
-
-				g := golden.New(t)
-				g.ShouldUpdate = update
-				g.AssertStruct(input.Name+"_ast", stmt)
-				g.AssertStruct(input.Name+"_errs", errs)
-			}
 		})
 	}
 }
