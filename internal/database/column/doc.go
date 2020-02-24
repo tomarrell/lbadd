d052c11e21526baaa2acf97454affb50f5a1c9f1
Tim Satke <tim.satke@gmail.com>
2020-02-24 16:35:32 +0100

    Remove lint errors and re-enable staticcheck in CI

---
+++ internal/database/column/doc.go
@@ -0,0 +1,4 @@
+// Package column describes columns inside the database. A column consists of a
+// type, type parameters and a few additional attributes, such as if the column
+// is nullable, if it is a primary key etc.
+package column
