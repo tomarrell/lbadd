9a677d680a8b76e4ac96f5891434972f696844ac
Tim Satke <tim.satke@gmail.com>
2020-02-25 21:41:16 +0100

    Remove implementation of deprecated interfaces

--- driver/conn.go
+++ driver/conn.go
@@ -7,10 +7,8 @@ import (
 
 var _ driver.Conn = (*Conn)(nil)
 var _ driver.ConnBeginTx = (*Conn)(nil)
-var _ driver.Execer = (*Conn)(nil)
 var _ driver.ExecerContext = (*Conn)(nil)
 var _ driver.Pinger = (*Conn)(nil)
-var _ driver.Queryer = (*Conn)(nil)
 var _ driver.QueryerContext = (*Conn)(nil)
 
 type Conn struct {
@@ -32,20 +30,12 @@ func (c *Conn) BeginTx(ctx context.Context, opts driver.TxOptions) (driver.Tx, e
 	return nil, nil // TODO(TimSatke): implement
 }
 
-func (c *Conn) Exec(query string, args []driver.Value) (driver.Result, error) {
-	return nil, nil // TODO(TimSatke): implement
-}
-
 func (c *Conn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
 	return nil, nil // TODO(TimSatke): implement
 }
 
 func (c *Conn) Ping(ctx context.Context) error {
 	return nil // TODO(TimSatke): implement
-}
-
-func (c *Conn) Query(query string, args []driver.Value) (driver.Rows, error) {
-	return nil, nil // TODO(TimSatke): implement
 }
 
 func (c *Conn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
