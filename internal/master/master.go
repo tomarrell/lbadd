4268f174c28549f9c10a01734cc0d84ddbecdc01
Tim Satke <tim.satke@gmail.com>
2020-03-13 14:56:56 +0100

    Fix linting issues

--- internal/master/master.go
+++ internal/master/master.go
@@ -8,11 +8,17 @@ import (
 	"github.com/tomarrell/lbadd/internal/executor"
 )
 
+// Master is a database master node.
+//
+//  m := master.New(log, executor)
+//  err := m.ListenAndServe(ctx, ":34213")
 type Master struct {
 	log  zerolog.Logger
 	exec executor.Executor
 }
 
+// New creates a new master node that is executing commands on the given
+// executor.
 func New(log zerolog.Logger, exec executor.Executor) *Master {
 	return &Master{
 		log:  log,
@@ -20,6 +26,10 @@ func New(log zerolog.Logger, exec executor.Executor) *Master {
 	}
 }
 
+// ListenAndServe starts the master node on the given address. The given context
+// must be used to stop the server, since there is no stop function. Canceling
+// the context or a context timeout will cause the server to attempt a graceful
+// shutdown.
 func (m *Master) ListenAndServe(ctx context.Context, addr string) error {
 	m.log.Info().
 		Str("addr", addr).
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -8,11 +8,17 @@ import (
 	"github.com/tomarrell/lbadd/internal/executor"
 )
 
+// Worker is a database worker node.
+//
+//  w := worker.New(log, exec)
+//  err := w.Connect(ctx, ":34213")
 type Worker struct {
 	log  zerolog.Logger
 	exec executor.Executor
 }
 
+// New creates a new worker with the given logger, that will replicate master
+// commands with the given executor.
 func New(log zerolog.Logger, exec executor.Executor) *Worker {
 	return &Worker{
 		log:  log,
@@ -20,6 +26,9 @@ func New(log zerolog.Logger, exec executor.Executor) *Worker {
 	}
 }
 
+// Connect connects the worker to a running and available master node. Use the
+// given context to close the connection and terminate the worker node.
+// Canceling the context will cause the worker to attempt a graceful shutdown.
 func (w *Worker) Connect(ctx context.Context, addr string) error {
 	w.log.Info().
 		Str("addr", addr).
