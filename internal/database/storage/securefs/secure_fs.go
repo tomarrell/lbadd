b9b1a2d6dc85d7ed0c0042c72cefded01f6f371d
Tim Satke <tim.satke@gmail.com>
2020-03-25 13:58:52 +0100

    Add secure filesystem abstraction
    Add a secure filesystem which implements afero.Fs,
    used to abstract the os filesystem. All files are
    held in memory, writes must be synced to disk manually,
    data in memory is held encrypted.

---
+++ internal/database/storage/securefs/secure_fs.go
@@ -0,0 +1,89 @@
+package securefs
+
+import (
+	"os"
+	"time"
+
+	"github.com/spf13/afero"
+)
+
+var _ afero.Fs = (*secureFs)(nil)
+
+type secureFs struct {
+	fs afero.Fs
+}
+
+// New creates a new secure fs, that only holds data in memory encrypted. Read
+// operations read bytes in passed in byte slices, which makes the bytes leave a
+// protected area. The given byte slice should also be in a protected area, but
+// this is the caller's responsibility. Files that are opened and/or created
+// with the returned Fs are 100% in memory. Writes must be synced to disk
+// manually by calling Sync().
+//
+//	foo.bar()
+//	foo.bar()
+func New(fs afero.Fs) afero.Fs {
+	return &secureFs{
+		fs: fs,
+	}
+}
+
+func (fs *secureFs) Create(name string) (afero.File, error) {
+	file, err := fs.fs.Create(name)
+	if err != nil {
+		return nil, err
+	}
+	return newSecureFile(file)
+}
+
+func (fs *secureFs) Mkdir(name string, perm os.FileMode) error {
+	return fs.fs.Mkdir(name, perm)
+}
+
+func (fs *secureFs) MkdirAll(path string, perm os.FileMode) error {
+	return fs.fs.MkdirAll(path, perm)
+}
+
+func (fs *secureFs) Open(name string) (afero.File, error) {
+	file, err := fs.fs.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return newSecureFile(file)
+}
+
+func (fs *secureFs) OpenFile(name string, flag int, perm os.FileMode) (afero.File, error) {
+	file, err := fs.fs.OpenFile(name, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+	return newSecureFile(file)
+}
+
+func (fs *secureFs) Remove(name string) error {
+	return fs.fs.Remove(name)
+}
+
+func (fs *secureFs) RemoveAll(path string) error {
+	return fs.fs.RemoveAll(path)
+}
+
+func (fs *secureFs) Rename(oldname, newname string) error {
+	return fs.fs.Rename(oldname, newname)
+}
+
+func (fs *secureFs) Stat(name string) (os.FileInfo, error) {
+	return fs.fs.Stat(name)
+}
+
+func (fs *secureFs) Name() string {
+	return "secure/" + fs.fs.Name()
+}
+
+func (fs *secureFs) Chmod(name string, mode os.FileMode) error {
+	return fs.fs.Chmod(name, mode)
+}
+
+func (fs *secureFs) Chtimes(name string, atime time.Time, mtime time.Time) error {
+	return fs.fs.Chtimes(name, atime, mtime)
+}
---
+++ internal/database/storage/securefs/secure_fs_test.go
@@ -0,0 +1,68 @@
+package securefs_test
+
+import (
+	"io"
+	"io/ioutil"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/tomarrell/lbadd/internal/database/storage/securefs"
+)
+
+func mustRead(t *testing.T, r io.Reader) []byte {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		assert.NoError(t, err)
+	}
+	return data
+}
+
+func TestSecureFs_FileOperations(t *testing.T) {
+	assert := assert.New(t)
+
+	underlying := afero.NewMemMapFs()
+	fs := securefs.New(underlying)
+
+	filename := "myfile.dat"
+	content := "hello, world!"
+	modContent := "hello, World!"
+
+	file, err := fs.Create(filename)
+	assert.NoError(err)
+	assert.Equal(filename, file.Name())
+
+	n, err := file.WriteString(content)
+	assert.NoError(err)
+	assert.Equal(len(content), n)
+
+	underlyingFile, err := underlying.Open(filename)
+	assert.NoError(err)
+	assert.Equal("", string(mustRead(t, underlyingFile)))
+	assert.NoError(underlyingFile.Close())
+
+	err = file.Sync()
+	assert.NoError(err)
+
+	underlyingFile, err = underlying.Open(filename)
+	assert.NoError(err)
+	assert.Equal(content, string(mustRead(t, underlyingFile)))
+	assert.NoError(underlyingFile.Close())
+
+	n, err = file.WriteAt([]byte("W"), 7)
+	assert.Equal(1, n)
+	assert.NoError(err)
+
+	underlyingFile, err = underlying.Open(filename)
+	assert.NoError(err)
+	assert.Equal(content, string(mustRead(t, underlyingFile)))
+	assert.NoError(underlyingFile.Close())
+
+	err = file.Sync()
+	assert.NoError(err)
+
+	underlyingFile, err = underlying.Open(filename)
+	assert.NoError(err)
+	assert.Equal(modContent, string(mustRead(t, underlyingFile)))
+	assert.NoError(underlyingFile.Close())
+}
