d175e8d348a788b9d7338b905ef624ed2b5aedd0
Tim Satke <tim.satke@gmail.com>
2020-05-07 22:19:44 +0200

    Fix typo

--- internal/network/server.go
+++ internal/network/server.go
@@ -19,8 +19,8 @@ type ConnHandler func(Conn)
 type Server interface {
 	io.Closer
 
-	// Open opens the server on the given address. To choose the
-	// server a random free port for you, specify a port ":0".
+	// Open opens the server on the given address. To make the server choose a
+	// random free port for you, specify a port ":0".
 	Open(string) error
 	// Listening can be used to get a signal when the server has allocated a
 	// port and is now actively listening for incoming connections.
