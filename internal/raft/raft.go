3c941932e5aedf7b43d2ba6087eb8ca93ee84c6f
Tim Satke <tim.satke@gmail.com>
2020-05-12 14:39:20 +0200

    Add OwnID to raft cluster

--- internal/raft/raft.go
+++ internal/raft/raft.go
@@ -28,6 +28,7 @@ type Server interface {
 
 // Cluster is a description of a cluster of servers.
 type Cluster interface {
+	OwnID() id.ID
 	Nodes() []network.Conn
 	Receive(context.Context) (network.Conn, message.Message, error)
 	Broadcast(context.Context, message.Message) error
