77d2a9a5073750bff2be6abd374cb7ccc5e9495b
Abhinav Kumar <abby37kapoor@gmail.com>
2020-05-19 09:59:16 +0530

    implemented appendEntriesResponse

--- internal/raft/append_entries.go
+++ internal/raft/append_entries.go
@@ -6,6 +6,50 @@ import "github.com/tomarrell/lbadd/internal/raft/message"
 // to the follower node. This function generates the response to be sent to the leader node.
 // This is the response to the contact by the leader to assert it's leadership.
 func AppendEntriesResponse(node *Node, req *message.AppendEntriesRequest) *message.AppendEntriesResponse {
+	success := true
+	leaderTerm := req.GetTerm()
+	nodePersistentState := node.PersistentState
+	nodeTerm := nodePersistentState.CurrentTerm
+	if nodeTerm > leaderTerm {
+		success = false
+	} else if req.GetPrevLogIndex() > int32(node.VolatileState.CommitIndex) {
+		success = false
+	} else if nodePersistentState.Log[req.PrevLogIndex].Term != req.GetPrevLogTerm() {
+		success = false
+	}
 
-	return nil
+	if !success {
+		return &message.AppendEntriesResponse{
+			Term:    nodeTerm,
+			Success: success,
+		}
+	}
+
+	entries := req.GetEntries()
+	if len(entries) > 0 { // if heartbeat, skip adding entries
+		nodePersistentState.mu.Lock()
+		if req.GetPrevLogIndex() < int32(node.VolatileState.CommitIndex) {
+			node.PersistentState.Log = node.PersistentState.Log[:req.GetPrevLogIndex()]
+		}
+		for _, entry := range entries {
+			node.PersistentState.Log = append(node.PersistentState.Log, *entry)
+		}
+		node.PersistentState.mu.Unlock()
+	}
+
+	if req.GetLeaderCommit() > int32(node.VolatileState.CommitIndex) {
+		nodeCommitIndex := int(req.GetLeaderCommit())
+		if int(req.GetLeaderCommit()) > len(node.PersistentState.Log) {
+			nodeCommitIndex = len(node.PersistentState.Log)
+		}
+		node.VolatileState.CommitIndex = nodeCommitIndex
+		// apply the log command & update lastApplied
+	}
+
+	return &message.AppendEntriesResponse{
+		Term:    nodeTerm,
+		Success: success,
+	}
+
 }
+
