7d1ec656413b7b322b049019c448d45e1dc74cd3
Abhinav Kumar <abby37kapoor@gmail.com>
2020-05-24 12:39:01 +0530

    comment added and assert message updated

--- internal/raft/append_entries_test.go
+++ internal/raft/append_entries_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/tomarrell/lbadd/internal/raft/message"
 )
 
+// TestAppendEntries
 func TestAppendEntries(t *testing.T) {
 	assert := assert.New(t)
 
@@ -30,6 +31,8 @@ func TestAppendEntries(t *testing.T) {
 	cluster.AddConnection(tcp1int)
 	cluster.AddConnection(tcp2int)
 
+	// Created a mock node with default values for PersistentState
+	// and Volatile State
 	node := &Node{
 		State: StateFollower.String(),
 		PersistentState: &PersistentState{
@@ -42,11 +45,13 @@ func TestAppendEntries(t *testing.T) {
 			CommitIndex: -1,
 			LastApplied: -1,
 		},
-		VolatileStateLeader: &VolatileStateLeader{},
 	}
 
-	entries := []*message.LogData{message.NewLogData(2, "execute cmd3"), message.NewLogData(2, "execute cmd4")}
-
+	entries := []*message.LogData{message.NewLogData(2,
+		"execute cmd3"), message.NewLogData(2, "execute cmd4")}
+	// Creating a mock msg AppendEntriesRequest with default values
+	// Leader commit specifies the Index of Log commited by leader and
+	// entries include msg LogData sent to nodes
 	msg := &message.AppendEntriesRequest{
 		Term:         1,
 		PrevLogIndex: -1,
@@ -57,21 +62,25 @@ func TestAppendEntries(t *testing.T) {
 
 	node.PersistentState.CurrentTerm = 3
 	res := AppendEntriesResponse(node, msg)
-	assert.False(res.Success, "Node Term is greater than leader term")
+	assert.False(res.Success, "Node Term is lesser than leader term")
 	msg.Term = 3
 	msg.PrevLogIndex = 3
 	node.VolatileState.CommitIndex = 2
 	res = AppendEntriesResponse(node, msg)
-	assert.False(res.Success, "Node Log Index is greater than leader commit Index")
+	assert.False(res.Success, "Node Log Index is lesser than"+
+		"leader commit Index")
 	msg.Term = 2
 	node.PersistentState.CurrentTerm = 2
 	msg.PrevLogIndex = 1
 	msg.PrevLogTerm = 1
 	node.VolatileState.CommitIndex = 1
-	node.PersistentState.Log = []*message.LogData{message.NewLogData(1, "execute cmd1"), message.NewLogData(1, "execute cmd2")}
+	node.PersistentState.Log = []*message.LogData{message.NewLogData(1,
+		"execute cmd1"), message.NewLogData(1, "execute cmd2")}
 	numberOfPersistentLog := len(node.PersistentState.Log)
 	res = AppendEntriesResponse(node, msg)
-	assert.True(res.Success, "Msg have been successfully applied to the node")
-	assert.Equal(node.PersistentState.CurrentTerm, res.GetTerm(), "Node have same term as leader")
-	assert.Equal(len(node.PersistentState.Log), numberOfPersistentLog+len(entries), "LogData has been appended successfully")
+	assert.True(res.Success, "Msg isn't appended to the node Logs")
+	assert.Equal(node.PersistentState.CurrentTerm, res.GetTerm(),
+		"Node doesn't have same term as leader")
+	assert.Equal(len(node.PersistentState.Log),
+		numberOfPersistentLog+len(entries), "LogData hasn't appended to the node ")
 }
