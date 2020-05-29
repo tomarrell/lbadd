a7569984aa75e0daf466d5f0efb6ff55952d44c3
Tim Satke <tim.satke@gmail.com>
2020-05-29 15:31:37 +0200

    Add drop tests

--- internal/compiler/simple_compiler_fixture_test.go
+++ internal/compiler/simple_compiler_fixture_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestCompileGolden(t *testing.T) {
 	t.Run("select", _TestCompileSelect)
 	t.Run("delete", _TestCompileDelete)
+	t.Run("drop", _TestCompileDrop)
 }
 
 func _TestCompileDelete(t *testing.T) {
@@ -18,16 +19,44 @@ func _TestCompileDelete(t *testing.T) {
 	}
 }
 
+func _TestCompileDrop(t *testing.T) {
+	tests := []string{
+		"DROP TABLE myTable",
+		"DROP TABLE IF EXISTS myTable",
+		"DROP TABLE mySchema.myTable",
+		"DROP TABLE IF EXISTS mySchema.myTable",
+		"DROP VIEW myView",
+		"DROP VIEW IF EXISTS myView",
+		"DROP VIEW mySchema.myView",
+		"DROP VIEW IF EXISTS mySchema.myView",
+		"DROP INDEX myIndex",
+		"DROP INDEX IF EXISTS myIndex",
+		"DROP INDEX mySchema.myIndex",
+		"DROP INDEX IF EXISTS mySchema.myIndex",
+		"DROP TRIGGER myTrigger",
+		"DROP TRIGGER IF EXISTS myTrigger",
+		"DROP TRIGGER mySchema.myTrigger",
+		"DROP TRIGGER IF EXISTS mySchema.myTrigger",
+	}
+	for _, test := range tests {
+		RunGolden(t, test)
+	}
+}
+
 func _TestCompileSelect(t *testing.T) {
 	tests := []string{
+		"SELECT * FROM myTable",
 		"SELECT * FROM myTable WHERE true",
-		"SELECT name FROM myTable WHERE true",
+		"SELECT * FROM myTable LIMIT 5",
+		"SELECT * FROM myTable LIMIT 5, 10",
+		"SELECT * FROM myTable LIMIT 5 OFFSET 10",
 		"SELECT DISTINCT * FROM myTable WHERE true",
 		"SELECT * FROM a, b WHERE true",
 		"SELECT * FROM a JOIN b WHERE true",
 		"SELECT * FROM a, b JOIN c WHERE true",
 		"SELECT name, amount * price AS total_price FROM items JOIN prices",
-		"SELECT col1 FROM a, b NATURAL JOIN c, d, e LEFT OUTER JOIN f CROSS JOIN g, h, i",
+		"SELECT AVG(price) AS avg_price FROM items LEFT JOIN prices",
+		"SELECT AVG(DISTINCT price) AS avg_price FROM items LEFT JOIN prices",
 	}
 	for _, test := range tests {
 		RunGolden(t, test)
