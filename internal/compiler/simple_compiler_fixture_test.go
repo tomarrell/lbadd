a9362e5313a5f9de1391fdb75e25f797994f93a5
Tim Satke <tim.satke@gmail.com>
2020-05-29 15:22:28 +0200

    Improve structure for golden testing

--- internal/compiler/simple_compiler_fixture_test.go
+++ internal/compiler/simple_compiler_fixture_test.go
@@ -2,29 +2,34 @@ package compiler
 
 import "testing"
 
-func TestCompileSelect(t *testing.T) {
-	tests := []struct {
-		name  string
-		input string
-	}{
-		{"simple select",
-			"SELECT * FROM myTable WHERE true"},
-		{"simple select",
-			"SELECT name FROM myTable WHERE true"},
-		{"select distinct",
-			"SELECT DISTINCT * FROM myTable WHERE true"},
-		{"select with implicit join",
-			"SELECT * FROM a, b WHERE true"},
-		{"select with explicit join",
-			"SELECT * FROM a JOIN b WHERE true"},
-		{"select with implicit and explicit join",
-			"SELECT * FROM a, b JOIN c WHERE true"},
-		{"select expression",
-			"SELECT name, amount * price AS total_price FROM items JOIN prices"},
-		{"select multiple joins",
-			"SELECT col1 FROM a, b NATURAL JOIN c, d, e LEFT OUTER JOIN f CROSS JOIN g, h, i"},
+func TestCompileGolden(t *testing.T) {
+	t.Run("select", _TestCompileSelect)
+	t.Run("delete", _TestCompileDelete)
+}
+
+func _TestCompileDelete(t *testing.T) {
+	tests := []string{
+		"DELETE FROM myTable",
+		"DELETE FROM mySchema.myTable",
+		"DELETE FROM myTable WHERE col1 == col2",
 	}
 	for _, test := range tests {
-		RunGolden(t, test.input, test.name)
+		RunGolden(t, test)
+	}
+}
+
+func _TestCompileSelect(t *testing.T) {
+	tests := []string{
+		"SELECT * FROM myTable WHERE true",
+		"SELECT name FROM myTable WHERE true",
+		"SELECT DISTINCT * FROM myTable WHERE true",
+		"SELECT * FROM a, b WHERE true",
+		"SELECT * FROM a JOIN b WHERE true",
+		"SELECT * FROM a, b JOIN c WHERE true",
+		"SELECT name, amount * price AS total_price FROM items JOIN prices",
+		"SELECT col1 FROM a, b NATURAL JOIN c, d, e LEFT OUTER JOIN f CROSS JOIN g, h, i",
+	}
+	for _, test := range tests {
+		RunGolden(t, test)
 	}
 }
