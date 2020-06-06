07c9ebc7b98b84c0b9d6e0ec27d73851d10d24f2
Tim Satke <tim.satke@gmail.com>
2020-06-06 18:08:13 +0200

    Add a few decoding function to the converter

--- internal/engine/converter/converter.go
+++ internal/engine/converter/converter.go
@@ -16,7 +16,7 @@ var (
 
 // bool
 
-// BoolToByte converts the given argument bool to a byte which is then returned.
+// BoolToByte converts the given bool to a byte which is then returned.
 func BoolToByte(v bool) byte {
 	if v {
 		return trueByte
@@ -24,64 +24,107 @@ func BoolToByte(v bool) byte {
 	return falseByte
 }
 
-// BoolToByteArray converts the given argument bool to a [1]byte is then
-// returned.
+// ByteToBool converts the given byte back to a bool.
+func ByteToBool(v byte) bool {
+	return v != falseByte
+}
+
+// BoolToByteArray converts the given bool to a [1]byte is then returned.
 func BoolToByteArray(v bool) [1]byte {
 	return [1]byte{BoolToByte(v)}
 }
 
-// BoolToByteSlice converts the given argument bool to a []byte which is then
-// returned.
+// ByteArrayToBool converts the given [1]byte back to a bool.
+func ByteArrayToBool(v [1]byte) bool {
+	return ByteToBool(v[0])
+}
+
+// BoolToByteSlice converts the given bool to a []byte which is then returned.
 func BoolToByteSlice(v bool) []byte {
 	arr := BoolToByteArray(v)
 	return arr[:]
 }
 
+// ByteSliceToBool converts the given []byte back to a bool.
+func ByteSliceToBool(v []byte) bool {
+	return ByteToBool(v[0])
+}
+
 // integral
 
-// Uint16ToByteArray converts the given argument uint16 to a [2]byte which is
-// then returned.
+// Uint16ToByteArray converts the given uint16 to a [2]byte which is then
+// returned.
 func Uint16ToByteArray(v uint16) (result [2]byte) {
 	byteOrder.PutUint16(result[:], v)
 	return
 }
 
-// Uint16ToByteSlice converts the given argument uint16 to a []byte which is
-// then returned.
+// ByteArrayToUint16 converts the given [2]byte back to a uint16.
+func ByteArrayToUint16(v [2]byte) uint16 {
+	return byteOrder.Uint16(v[:])
+}
+
+// Uint16ToByteSlice converts the given uint16 to a []byte which is then
+// returned.
 func Uint16ToByteSlice(v uint16) (result []byte) {
 	result = make([]byte, 2)
 	byteOrder.PutUint16(result, v)
 	return
 }
 
-// Uint32ToByteArray converts the given argument uint32 to a [4]byte which is
-// then returned.
+// ByteSliceToUint16 converts the given []byte back to a uint16.
+func ByteSliceToUint16(v []byte) uint16 {
+	return byteOrder.Uint16(v)
+}
+
+// Uint32ToByteArray converts the given uint32 to a [4]byte which is then
+// returned.
 func Uint32ToByteArray(v uint32) (result [4]byte) {
 	byteOrder.PutUint32(result[:], v)
 	return
 }
 
-// Uint32ToByteSlice converts the given argument uint32 to a []byte which is
-// then returned.
+// ByteArrayToUint32 converts the given [4]byte back to a uint32.
+func ByteArrayToUint32(v [4]byte) uint32 {
+	return byteOrder.Uint32(v[:])
+}
+
+// Uint32ToByteSlice converts the given uint32 to a []byte which is then
+// returned.
 func Uint32ToByteSlice(v uint32) (result []byte) {
 	result = make([]byte, 4)
 	byteOrder.PutUint32(result, v)
 	return
 }
 
-// Uint64ToByteArray converts the given argument uint64 to a [8]byte which is
-// then returned.
+// ByteSliceToUint32 converts the given []byte back to a uint32.
+func ByteSliceToUint32(v []byte) uint32 {
+	return byteOrder.Uint32(v)
+}
+
+// Uint64ToByteArray converts the given uint64 to a [8]byte which is then
+// returned.
 func Uint64ToByteArray(v uint64) (result [8]byte) {
 	byteOrder.PutUint64(result[:], v)
 	return
 }
 
-// Uint64ToByteSlice converts the given argument uint64 to a []byte which is
-// then returned.
+// ByteArrayToUint64 converts the given [8]byte back to a uint64.
+func ByteArrayToUint64(v [8]byte) uint64 {
+	return byteOrder.Uint64(v[:])
+}
+
+// Uint64ToByteSlice converts the given uint64 to a []byte which is then
+// returned.
 func Uint64ToByteSlice(v uint64) (result []byte) {
 	result = make([]byte, 8)
 	byteOrder.PutUint64(result, v)
 	return
+}
+
+// ByteSliceToUint64 converts the given []byte back to a uint64.
+func ByteSliceToUint64(v []byte) uint64 {
+	return byteOrder.Uint64(v)
 }
 
 // fractal
@@ -148,8 +191,12 @@ func Complex128ToByteSlice(v complex128) (result []byte) {
 
 // variable-size
 
-// StringToByteSlice converts the given argument string a []byte which is then
-// returned.
+// StringToByteSlice converts the given string a []byte which is then returned.
 func StringToByteSlice(v string) []byte {
 	return []byte(v)
+}
+
+// ByteSliceToString converts the given []byte back to a string.
+func ByteSliceToString(v []byte) string {
+	return string(v)
 }
