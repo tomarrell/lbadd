154327f7cec4d3757d6583e1d7e2319385974d94
Tim Satke <tim.satke@gmail.com>
2020-06-23 12:44:11 +0200

    Add more validating functions

--- internal/engine/storage/validator.go
+++ internal/engine/storage/validator.go
@@ -34,11 +34,19 @@ func (v *Validator) Validate() error {
 	}
 	v.info = stat
 
-	if err := v.validateIsFile(); err != nil {
-		return fmt.Errorf("is file: %w", err)
+	validations := []struct {
+		name      string
+		validator func() error
+	}{
+		{"is file", v.validateIsFile},
+		{"size", v.validateSize},
+		{"page count", v.validatePageCount},
 	}
-	if err := v.validateSize(); err != nil {
-		return fmt.Errorf("size: %w", err)
+
+	for _, validation := range validations {
+		if err := validation.validator(); err != nil {
+			return fmt.Errorf("%v: %w", validation.name, err)
+		}
 	}
 
 	return nil
@@ -58,6 +66,32 @@ func (v Validator) validateSize() error {
 	size := v.info.Size()
 	if size%page.Size != 0 {
 		return fmt.Errorf("invalid file size, must be multiple of page size (=%v), but was %v", page.Size, size)
+	}
+	return nil
+}
+
+func (v Validator) validatePageCount() error {
+	mgr, err := NewPageManager(v.file)
+	if err != nil {
+		return fmt.Errorf("new page manager: %w", err)
+	}
+
+	headerPage, err := mgr.ReadPage(HeaderPageID)
+	if err != nil {
+		return fmt.Errorf("read header page: %w", err)
+	}
+
+	val, ok := headerPage.Cell([]byte(HeaderPageCount))
+	if !ok {
+		return fmt.Errorf("no page count header field in header page")
+	}
+	pageCountField, ok := val.(page.RecordCell)
+	if !ok {
+		return fmt.Errorf("page count cell is not a record cell (%v)", val.Type())
+	}
+	pageCount := byteOrder.Uint64(pageCountField.Record)
+	if int64(pageCount) != v.info.Size()/page.Size {
+		return fmt.Errorf("page count does not match file size (pageCount=%v,size=%v,expected count=%v)", pageCount, v.info.Size(), v.info.Size()/page.Size)
 	}
 	return nil
 }
