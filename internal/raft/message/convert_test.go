22600e1f7461fe07446e13cacfb7d097347f9825
SUMUKHA-PK <sumukhapk46@gmail.com>
2020-06-30 11:10:17 +0530

    this commits adds implementation of converting message to command types and moves simpleServer to SimpleServer while returning the same for raft.NewServer instead of an interface

--- internal/raft/message/convert_test.go
+++ internal/raft/message/convert_test.go
@@ -1,11 +1,5 @@
 package message
 
-import (
-	"testing"
-
-	"github.com/tomarrell/lbadd/internal/compiler/command"
-)
-
 // var commandToMessageTests = []struct {
 // 	in  command.Command
 // 	out Message
@@ -44,48 +38,48 @@ import (
 // 	}
 // }
 
-var messageToCommandTests = []struct {
-	in  Message
-	out command.Command
-}{
-	{
-		&Command_Scan{
-			Table: &SimpleTable{
-				Schema:  "mySchema",
-				Table:   "myTable",
-				Alias:   "myAlias",
-				Indexed: true,
-				Index:   "myIndex",
-			},
-		},
-		command.Scan{
-			Table: command.SimpleTable{
-				Schema:  "mySchema",
-				Table:   "myTable",
-				Alias:   "myAlias",
-				Indexed: true,
-				Index:   "myIndex",
-			},
-		},
-	},
-	// {
-	// 	&Command_Select{
-	// 		Filter: &Expr{
-	// 			Expr: ,
-	// 		},
-	// 	},
-	// 	command.Select{},
-	// },
-}
+// var messageToCommandTests = []struct {
+// 	in  Message
+// 	out command.Command
+// }{
+// 	{
+// 		&Command_Scan{
+// 			Table: &SimpleTable{
+// 				Schema:  "mySchema",
+// 				Table:   "myTable",
+// 				Alias:   "myAlias",
+// 				Indexed: true,
+// 				Index:   "myIndex",
+// 			},
+// 		},
+// 		command.Scan{
+// 			Table: command.SimpleTable{
+// 				Schema:  "mySchema",
+// 				Table:   "myTable",
+// 				Alias:   "myAlias",
+// 				Indexed: true,
+// 				Index:   "myIndex",
+// 			},
+// 		},
+// 	},
+// 	// {
+// 	// 	&Command_Select{
+// 	// 		Filter: &Expr{
+// 	// 			Expr: ,
+// 	// 		},
+// 	// 	},
+// 	// 	command.Select{},
+// 	// },
+// }
 
-func Test_MessageToCommand(t *testing.T) {
-	t.SkipNow()
-	for _, tt := range messageToCommandTests {
-		t.Run(tt.in.Kind().String(), func(t *testing.T) {
-			msg := ConvertMessageToCommand(tt.in)
-			if msg != tt.out {
-				t.Errorf("got %q, want %q", msg, tt.out)
-			}
-		})
-	}
-}
+// func Test_MessageToCommand(t *testing.T) {
+// 	t.SkipNow()
+// 	for _, tt := range messageToCommandTests {
+// 		t.Run(tt.in.Kind().String(), func(t *testing.T) {
+// 			msg := ConvertMessageToCommand(tt.in)
+// 			if msg != tt.out {
+// 				t.Errorf("got %q, want %q", msg, tt.out)
+// 			}
+// 		})
+// 	}
+// }
